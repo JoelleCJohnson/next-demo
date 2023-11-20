9e3e61ce6bfda61d9827eca9603c688055b8bd4a
Joelle  Johnson <joellej221@gmail.com>
2023-11-20 12:23:10 -0500

    Used Next fetch to get data before rendering

---
+++ app/affirmations/new/page.js
@@ -0,0 +1,24 @@
+import AffirmationCard from "@/components/AffirmationCard"
+
+const getData = async () => {
+    const res = await fetch('https://affirm-bc-api.web.app/affirmations')
+    return res.json()
+}
+
+
+export default async function NewAffirm() {
+
+    const affirmations = await getData()
+
+    return (
+        <>
+            <h1 className="text-2xl font-bold dark:text-sky-300 text-sky-600">
+                New Affirmations</h1>
+                <section className="container mx-auto px-5 py-12 flex flex-wrap">
+                    {affirmations.map(item => (
+                        <AffirmationCard key={item.id} item={item} />
+                    ))}
+                </section>
+        </>
+    )
+}
